a9492cf0d2f63dc6000688828a310a6cbcef7f80
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-22 17:51:05 -0500

    Fusiona el Frontend y el Backend

--- core/views.py
+++ core/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Inicio")
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse("Historia")
+    return render(request, "core/about.html")
 
 def services(request):
-    return HttpResponse("Servicios")
+    return render(request, "core/services.html")
 
 def store(request):
-    return HttpResponse("Visitanos")
+    return render(request, "core/store.html")
 
 def contact(request):
-    return HttpResponse("Contacto")
+    return render(request, "core/contact.html")
 
 def blog(request):
-    return HttpResponse("Blog")
+    return render(request, "core/blog.html")
 
 def sample(request):
-    return HttpResponse("Sample")
+    return render(request, "core/sample.html")
