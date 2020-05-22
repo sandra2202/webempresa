bb032ba7b662852e0e029cd3697eef239970e60c
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-22 18:49:45 -0500

    Tercera App Modelo y Admin

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
 
 urlpatterns = [
     #Paths del core
     path('', include('core.urls')),
-
     #Paths del admin
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
