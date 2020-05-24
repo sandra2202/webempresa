18b65bcbad0b86cf1c6ae8f73143d8627b287c0c
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-23 22:47:30 -0500

    Ordenacion y edicion derecta de paginas

--- pages/models.py
+++ pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="Título", max_length=200)
     content = models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
 
     class Meta:
         verbose_name = "página"
         verbose_name_plural = "páginas"
-        ordering = ['title']
+        ordering = ['order','title']
 
     def __str__(self):
         return self.title
